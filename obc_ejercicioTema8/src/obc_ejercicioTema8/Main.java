6717b62a52520772432c16cea5f90d36887bf3b7
Jose Soto <jose.soto.developer@gmail.com>
2023-05-02 23:45:44 +0200

    Commit final ejercicio

--- obc_ejercicioTema8/src/obc_ejercicioTema8/Main.java
+++ obc_ejercicioTema8/src/obc_ejercicioTema8/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Persona persona = new Persona();
+		
+		persona.setEdad(35);
+		persona.setNombre("Carlos");
+		persona.setTelefono("656656656");
+		
+		System.out.println(persona.getNombre());
+		System.out.println(persona.getEdad());
+		System.out.println(persona.getTelefono());
 	}
 
 }
